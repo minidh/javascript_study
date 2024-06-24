ea83d2c81217ecbdb6d3466889310631f86db2ed
minidh <m_dohui59@naver.com>
2024-06-24 16:22:26 +0900

    space_invader

--- 스페이스 인베이더/app.js
+++ 스페이스 인베이더/app.js
@@ -12,12 +12,18 @@ const stopBtn = document.querySelector("#stopBtn");
 const display = document.querySelector("#display");
 
 let playerLoc = stageSize*(stageSize-2)+7 // 202;
+let invaderLoc = [
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+    15, 16 ,17, 18, 19, 20, 21, 22, 23, 24,
+    30, 31, 32, 33, 34, 35, 36, 37, 38, 39
+]
+let gameInterval;
+
+let bulletLoc = playerLoc;
 
 function makePlayer() {
     stage[playerLoc].classList.add("player");
 }
-makePlayer()
-
 function movePlayer(e) {
     stage[playerLoc].classList.remove("player");
     switch (e.keyCode) {
@@ -32,13 +38,6 @@ function movePlayer(e) {
 }
 
 document.addEventListener("keyup", movePlayer)
-
-let invaderLoc = [
-    0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
-    15, 16 ,17, 18, 19, 20, 21, 22, 23, 24,
-    30, 31, 32, 33, 34, 35, 36, 37, 38, 39
-]
-let gameInterval;
 
 function makeInvader() {
     invaderLoc.forEach(function(invader) {
@@ -57,12 +56,21 @@ function moveInvader() {
 }
 
 function gameStart() {
+    stage[playerLoc].classList.remove("player");
+    invaderLoc.forEach(function(invader) {
+        stage[invader].classList.remove("invader"); });
+    display.innerText = ""
+    playerLoc = stageSize*(stageSize-2)+7 // 202
+    invaderLoc = [
+        0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+        15, 16 ,17, 18, 19, 20, 21, 22, 23, 24,
+        30, 31, 32, 33, 34, 35, 36, 37, 38, 39]
     makePlayer();
     makeInvader();
     displayStatus();
     gameInterval = setInterval(moveInvader, 1000);
     document.addEventListener("keyup", movePlayer);
-    gameRun();
+    gameRun(); 
 }
 
 function gameStop() {
@@ -77,6 +85,35 @@ function gameRun() {
 function displayStatus() {
     display.innerText = invaderLoc.length + "/" + invaderLoc.length
 }
+
+function moveBullet() {
+    stage[bulletLoc].classList.remove("bullet");
+    bulletLoc -= stageSize;
+    stage[bulletLoc].classList.add("bullet")
+}
+bulletId = setInterval(moveBullet, 500);
+function shoot(e) {
+    let id;
+    let bulletLoc = playerLoc;
+    function moveBullet() {
+        stage[bulletLoc].classList.remove("bullet");
+        bulletLoc -= stageSize;
+        if (bulletLoc < 0) {
+            clearInterval(id);
+        }
+        stage[bulletLoc].classList.add("bullet");
+    }
+    if (e.key === "ArrowUp") {
+        id = setInterval(moveBullet, 300)
+    stage[bulletLoc].classList.add("bullet");
+    if(stage[bulletLoc].classList.contains("invader")) {
+       stage[bulletLoc].classList.remove("invader");
+       stage[bulletLoc].classList.remove("bullet");
+       stage[bulletLoc].classList,add("boom");
+    }
+    }
+}
+document.addEventListener("keydown", shoot);
 
 startBtn.addEventListener("click", gameStart);
 stopBtn.addEventListener("click", gameStop);
